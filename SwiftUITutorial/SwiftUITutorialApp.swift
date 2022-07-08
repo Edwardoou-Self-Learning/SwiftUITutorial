ac8ee0a45e1c844186f816fac0f50422044bf957
edwardoou <67031155+edwardoou@users.noreply.github.com>
2022-07-08 10:12:06 -0500

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Alex Ramos on 8/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
